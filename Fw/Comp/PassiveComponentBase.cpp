d832a21412c66f2cc27132ae50baeb7179ea6dad
andres395 <74679318+andres395@users.noreply.github.com>
2023-03-29 09:23:01 -0500

    Initialize master

---
+++ Fw/Comp/PassiveComponentBase.cpp
@@ -0,0 +1,45 @@
+#include <Fw/Comp/PassiveComponentBase.hpp>
+#include <Fw/Types/Assert.hpp>
+#include <FpConfig.hpp>
+
+#include <stdio.h>
+
+namespace Fw {
+
+    PassiveComponentBase::PassiveComponentBase(const char* name) : Fw::ObjBase(name), m_idBase(0), m_instance(0) {
+    }
+    
+#if FW_OBJECT_TO_STRING == 1 && FW_OBJECT_NAMES == 1
+    void PassiveComponentBase::toString(char* buffer, NATIVE_INT_TYPE size) {
+        FW_ASSERT(buffer);
+        (void)snprintf(buffer, size, "Comp: %s", this->m_objName);
+        // null terminate
+        buffer[size-1] = 0;
+    }
+#endif
+    
+    PassiveComponentBase::~PassiveComponentBase(void) {
+    }
+    
+    void PassiveComponentBase::init(NATIVE_INT_TYPE instance) {
+        ObjBase::init();
+        this->m_instance = instance;
+    }
+
+    NATIVE_INT_TYPE PassiveComponentBase::getInstance(void) const {
+        return this->m_instance;
+    }
+
+    void PassiveComponentBase ::
+      setIdBase(const U32 idBase)
+    {
+      this->m_idBase = idBase;
+    }
+
+    U32 PassiveComponentBase ::
+      getIdBase(void) const
+    {
+      return this->m_idBase;
+    }
+
+}
